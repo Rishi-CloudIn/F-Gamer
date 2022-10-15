2fc29556fb0b03787749f6af77cebd4641ed4979
Rishi-CloudIn <rishi@cloudin.email>
2022-10-15 18:44:34 +0530

    login added

--- src/components/forms/login-form/index.jsx
+++ src/components/forms/login-form/index.jsx
@@ -1,27 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
 import { StaticImage } from "gatsby-plugin-image";
 import Button from "../../../components/shared/button";
 import { Link } from "gatsby";
+import { useForm } from "react-hook-form";
 
 const LoginForm = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        trigger,
+        reset,
+    } = useForm();
+
+    const [user, setUser] = useState();
+
+    const onInputChange = (e) => {
+        setUser({ ...user, [e.target.name]: e.target.value });
+    };
+
+    const onSubmit = (data) => {
+        console.log("data", data);
+
+        if (data.email === "admin@gmail.com" && data.password === "admin") {
+            window.location = "https://coincodex.com/convert/load/tether/";
+        } else {
+        }
+        reset();
+    };
+
+    const onError = (errors) => {
+        console.log(errors, alert("Enter all inputs :)"));
+    };
+
     return (
-        <form className="form-login mt-10" action="#">
+        <form
+            className="form-login mt-10"
+            action="#"
+            onSubmit={handleSubmit(onSubmit, onError)}
+        >
             <div className="single-fild">
                 <input
                     className="px-6 h-14 mb-6 border-secondary-90 bg-secondary-100 hover:border-primary transition-all border-2 border-solid block rounded-md w-full focus:outline-none"
                     type="email"
                     placeholder="E-mail"
+                    name="email"
+                    onChange={(e) => onInputChange(e)}
+                    {...register("email", {
+                        required: "Email is Required",
+                        pattern: {
+                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                            message: "Invalid email address",
+                        },
+                    })}
+                    onKeyUp={() => {
+                        trigger("email");
+                    }}
                 />
+                {errors.email && (
+                    <small className="text-danger">
+                        {errors.email.message}
+                    </small>
+                )}
             </div>
             <div className="single-fild">
                 <input
                     className="px-6 h-14 mb-6 border-secondary-90 bg-secondary-100 hover:border-primary transition-all border-2 border-solid block rounded-md w-full focus:outline-none"
                     type="password"
                     placeholder="password"
+                    name="password"
+                    onChange={(e) => onInputChange(e)}
+                    {...register("password", {
+                        required: "password is Required",
+                        pattern: {
+                            value: /^[a-zA-Z0-9]+([a-zA-Z0-9](_|-| )[a-zA-Z0-9])*[a-zA-Z0-9]+$/,
+                            message: "Invalid password",
+                        },
+                    })}
+                    onKeyUp={() => {
+                        trigger("password");
+                    }}
                 />
+                {errors.password && (
+                    <small className="text-danger">
+                        {errors.password.message}
+                    </small>
+                )}
             </div>
             <div className="button text-center">
-                <Button className="text-white">
+                <Button className="text-white" type="submit">
                     Login
                     <StaticImage
                         className="align-middle ml-3 transition-all group-hover:ml-5"
--- src/components/logo/index.jsx
+++ src/components/logo/index.jsx
@@ -1,11 +1,15 @@
 import { Link } from "gatsby";
 import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
-
+import "./logo.css";
 const Logo = () => {
     return (
         <Link to="/" className="block">
-            <StaticImage src="../../data/images/logo.webp" alt="Bonx" />
+            <StaticImage
+                src="../../data/images/gamer_logo.png"
+                className="gamer_logo"
+                alt="Bonx"
+            />
         </Link>
     );
 };
